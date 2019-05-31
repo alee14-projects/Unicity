ac5d2f813801933f7044c0fcfd15a6d79df78958
Alee <Alee14498@gmail.com>
2019-05-31 19:38:47 -0400

    Camera is now working

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -17,8 +17,6 @@
 *    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 *
 *********************************************************************************/
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
