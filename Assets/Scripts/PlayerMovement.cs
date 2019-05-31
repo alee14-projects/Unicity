3476fd1bff0105bf003de9edecb589c889ecfb0c
Alee <Alee14498@gmail.com>
2019-05-31 17:31:14 -0400

    Trying to get movement working

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,29 +1,38 @@
-﻿using System.Collections;
+﻿/*******************************************************************************
+*
+*    Project SimLife: A Sims clone written in Unity C#
+*    Copyright (C) 2019 AleeCorp + Software Elevated
+*
+*    This program is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    (at your option) any later version.
+*
+*    This program is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*
+*********************************************************************************/
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public LayerMask whatCanBeClickedOn;
-
-    private UnityEngine.AI.NavMeshAgent myAgent;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        myAgent = GetComponent <UnityEngine.AI.NavMeshAgent> ();
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0)) {
-            Ray myRay = Camera.main.ScreenPointToRay (Input.mousePosition);
+         if (Input.GetButtonDown("Fire1"))
+            {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hitInfo;
-
-            if(Physics.Raycast (myRay, out hitInfo, 100, whatCanBeClickedOn)) {
-                myAgent.SetDestination (hitInfo.point);
+            if (Physics.Raycast(ray, out hitInfo))
+            {
+                GetComponent<NavMeshAgent>().SetDestination(hitInfo.point);
             }
         }
     }
