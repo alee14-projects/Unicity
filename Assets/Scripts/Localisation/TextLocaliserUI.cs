849c109ae26960be15722f01b9ea8230f4a2efc2
Alee <Alee14498@gmail.com>
2019-06-10 12:13:55 -0400

    Localisation (Working progress)

---
+++ Assets/Scripts/Localisation/TextLocaliserUI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+[RequireComponent(typeof(TextMeshProUGUI))]
+public class TextLocaliserUI : MonoBehaviour
+{
+    TextMeshProUGUI textField;
+
+    public string key;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textField = GetComponent<TextMeshProUGUI>();
+        string value = LocalisationSystem.GetLocalisedValue(key);
+        textField.text = value;
+    }
+
+}
