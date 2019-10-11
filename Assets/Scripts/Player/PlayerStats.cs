93b5f588e3cb644f61e002932d61f2c6cdab87ab
Andrew Lee <alee14498@gmail.com>
2019-10-11 18:16:34 -0400

    We finally got rid of GPL

--- Assets/Scripts/Player/PlayerStats.cs
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -1,20 +1,10 @@
 ﻿/*******************************************************************************
 *
-*    Project SimLife: A Sims clone written in Unity C#
-*    Copyright (C) 2019 AleeCorp + Software Elevated
+*    Unicity (Project SimLife): A Sims clone written in Unity C#
+*    Copyright (C) 2019 Unicity Development Team
 *
-*    This program is free software: you can redistribute it and/or modify
-*    it under the terms of the GNU General Public License as published by
-*    the Free Software Foundation, either version 3 of the License, or
-*    (at your option) any later version.
-*
-*    This program is distributed in the hope that it will be useful,
-*    but WITHOUT ANY WARRANTY; without even the implied warranty of
-*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*    GNU General Public License for more details.
-*
-*    You should have received a copy of the GNU General Public License
-*    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*    This software is protected by the copyright and licensing rights held
+*    by the Unicity Development Team. (2019)
 *
 *********************************************************************************/
 using System.Collections;
